4f5a64fe143d83c88b6c6d3b9b835872655ad3bf
Pedro <pedro_rjsg@hotmail.com>
2024-07-03 21:44:45 -0300

    Python

---
+++ python/calculadora.py
@@ -0,0 +1,12 @@
+x = input("digite o valor 1: ")
+y = input("digite o valor 2: ")
+
+print(int(x) + int(y))
+# z = int(x) + int(y)
+
+#print(z)
+print(round(float(x) / float(y),2))
+
+valor = int(x) + int(y)
+
+print(f"{valor:,}")
---
+++ python/hello.py
@@ -0,0 +1,9 @@
+# ler o nome de usuario
+nome = input("Qual é o seu nome? ").strip().title()
+
+#nome = nome.strip().title().capitalize()
+
+pimeiro, sobrenome = nome.split(" ")
+# exibir uma mensagem na tela de saudação
+print(f"Olá, {nome}")
+print(sobrenome)
